3e901c0622ea4604b9a2ff327974b00543c775d4
Mohamed Taj-Eldin Ahmed <77786672+DaTaj-ai@users.noreply.github.com>
2025-05-22 08:30:35 +0300

    Time: 22 ms (20.04%), Space: 49.2 MB (5.8%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.kt
+++ 0125-valid-palindrome/0125-valid-palindrome.kt
@@ -1,18 +1,5 @@
 class Solution {
     fun isPalindrome(s: String): Boolean {
-            var lowerCaseWords = ""
-    for(i in s ){
-        if (i.toInt() >= 'A'.toInt() && i.toInt() <= 'Z'.toInt() || i.toInt() >= 'a'.toInt() && i.toInt() <= 'z'.toInt()|| i.isDigit()){
-            lowerCaseWords+=i.lowercase()
-        }
-    }
-
-    if (lowerCaseWords == lowerCaseWords.reversed()){
-        return true 
-    }
-    else{
-        return  false 
-    }
-        
-    }
-}
+        return s.filter { it.isLetter() || it.isDigit() }
+            .map { it.lowercase() }
+            .let { it == it.reversed()}}}
